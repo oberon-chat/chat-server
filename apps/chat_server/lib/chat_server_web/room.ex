e19b88fb6de01eeb13cd00736abf2b33f94d0bf7
Chris Laskey <contact@chrislaskey.com>
2017-09-13 18:36:39 -0400

    Track room name in state, publish name when deleted

--- apps/chat_server/lib/chat_server_web/room.ex
+++ apps/chat_server/lib/chat_server_web/room.ex
@@ -3,18 +3,31 @@ defmodule ChatServerWeb.Room do
 
   alias ChatServerWeb.RoomSupervisor
 
-  defstruct messages: []
-
-  def start(room) do
-    Supervisor.start_child(RoomSupervisor, [room])
+  defmodule State do
+    defstruct [:name, messages: []]
   end
 
-  def start_link(room) do
-    GenServer.start_link(__MODULE__, room)
+  def start(name) do
+    Supervisor.start_child(RoomSupervisor, [name])
+  end
+
+  def start_link(name) do
+    GenServer.start_link(__MODULE__, name)
   end
 
   def stop(room_pid) do
-    Supervisor.terminate_child(RoomSupervisor, room_pid)
+    name = get_name(room_pid)
+
+    with :ok <- Supervisor.terminate_child(RoomSupervisor, room_pid) do
+      ChatPubSub.broadcast! "rooms", "room:deleted", %{room: name}
+    end
+  end
+
+  def get_name(%Phoenix.Socket{} = socket) do
+    GenServer.call(socket.assigns[:room_pid], {:get_name})
+  end
+  def get_name(pid) do
+    GenServer.call(pid, {:get_name})
   end
 
   def get_message(%Phoenix.Socket{} = socket, id) do
@@ -52,8 +65,12 @@ defmodule ChatServerWeb.Room do
     GenServer.cast(pid, {:create_message, msg})
   end
 
-  def init(_room) do
-    {:ok, %ChatServerWeb.Room{}}
+  def init(name) do
+    {:ok, %State{name: name}}
+  end
+
+  def handle_call({:get_name}, _from, %{name: name} = state) do
+    {:reply, %{name: name}, state}
   end
 
   def handle_call({:get_message, id}, _from, %{messages: messages} = state) do
