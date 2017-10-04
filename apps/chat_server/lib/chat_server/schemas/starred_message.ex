94e448f74815ef5f1c8993895c5cc0543ece0910
Hailey Willis <yourFriendWes@users.noreply.github.com>
2017-10-03 18:09:09 -0700

    Create Schema for StarredMessages

---
+++ apps/chat_server/lib/chat_server/schemas/starred_message.ex
@@ -0,0 +1,31 @@
+defmodule ChatServer.Schema.StarredMessage do
+  use ChatServer.Schema
+
+  @derive {Poison.Encoder, except: [:__meta__]}
+
+  @primary_key false
+
+  schema "starred_messages" do
+    belongs_to :user, ChatServer.Schema.User
+    belongs_to :message, ChatServer.Schema.Message
+    timestamps()
+  end
+
+  def create(params) do
+    %StarredMessage{}
+    |> changeset(params)
+    |> Repo.insert
+  end
+
+  def delete(%StarredMessage{} = starred_message) do
+    Repo.delete(starred_message)
+  end
+
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:message_id, :user_id])
+    |> assoc_constraint(:message)
+    |> assoc_constraint(:user)
+  end
+end
