705c0a4a9279f55db29d9fb815144cf0fdf40a57
Hailey Willis <yourFriendWes@users.noreply.github.com>
2017-10-24 15:55:15 -0700

    Change method calls that load StarredMessage

--- apps/chat_server/lib/chat_server/contexts/starred_messages/delete_starred_message.ex
+++ apps/chat_server/lib/chat_server/contexts/starred_messages/delete_starred_message.ex
@@ -5,8 +5,9 @@ defmodule ChatServer.DeleteStarredMessage do
 
   def call(message_id, user) do
     message = Schema.Message.get(message_id)
-    starred_message = Schema.StarredMessage.find_by_message_and_user(user, message)
-    star_id = starred_message.id
+    starred_message = Schema.StarredMessage.find_by_message_and_user(message, user)
+    star_id = Map.get(starred_message, :id)
+
     with {:ok, _record} <- Schema.StarredMessage.delete(starred_message) do
         {:ok, star_id}
     else
@@ -15,5 +16,4 @@ defmodule ChatServer.DeleteStarredMessage do
           {:error, "Error deleting starred message"}
     end
   end
-
 end
