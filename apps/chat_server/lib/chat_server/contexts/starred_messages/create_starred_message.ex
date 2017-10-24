9bf95d0fe393b833dd82334b2dd8966460613d8e
Hailey Willis <yourFriendWes@users.noreply.github.com>
2017-10-24 15:54:12 -0700

    Format data sent to StarMessage.create to be map
    - w

--- apps/chat_server/lib/chat_server/contexts/starred_messages/create_starred_message.ex
+++ apps/chat_server/lib/chat_server/contexts/starred_messages/create_starred_message.ex
@@ -5,7 +5,7 @@ defmodule ChatServer.CreateStarredMessage do
 
   def call(message_id, user) do
     message = Schema.Message.get(message_id)
-    with {:ok, record} <- Schema.StarredMessage.create(%{user: user, message: message}) do
+    with {:ok, record} <- create_record(message, user) do
         {:ok, Schema.StarredMessage.preload(record, [:message, :user])}
     else
       error ->
@@ -14,4 +14,15 @@ defmodule ChatServer.CreateStarredMessage do
     end
   end
 
+  defp create_record(message, user) do
+    message
+    |> filter_params(user)
+    |> Schema.StarredMessage.create
+  end
+
+  defp filter_params(message, user) do
+    %{}
+    |> Map.put("message_id", Map.get(message, :id, nil))
+    |> Map.put("user_id", Map.get(user, :id, nil))
+  end
 end
