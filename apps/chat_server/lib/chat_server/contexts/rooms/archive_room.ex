f32c5ad5e0fd60c8492e017e7ed7e332f68ffa5e
Jack Hefner <jmhthethird@gmail.com>
2017-10-27 09:46:45 -0700

    add archive_room context object. heavily based off of create_room object

---
+++ apps/chat_server/lib/chat_server/contexts/rooms/archive_room.ex
@@ -0,0 +1,38 @@
+defmodule ChatServer.ArchiveRoom do
+  require Logger
+
+  alias ChatServer.Schema
+
+  defmodule State do
+    @derive {Poison.Encoder, only: [room: [:id, :slug, :name]]}
+
+    defstruct [:room]
+  end
+
+  def call(params \\ %{}, _user) do
+    Logger.info "Archiving room " <> inspect(params)
+
+    # TODO verify user is allowed to archive room of that type
+
+    with {:ok, record} <- archive_room(params),
+         :ok <- broadcast_archive(record) do
+      {:ok, record}
+    else
+      _ -> :error
+    end
+  end
+
+  defp archive_room(params) do
+    changeset = params
+    |> Map.take(["id", "name"])
+    |> Map.put("state", "archived")
+
+    Schema.Room.update(params, changeset)
+  end
+
+  defp broadcast_archive(room) do
+    event = %State{room: room}
+
+    ChatPubSub.broadcast! "events", "room:archived", event
+  end
+end
