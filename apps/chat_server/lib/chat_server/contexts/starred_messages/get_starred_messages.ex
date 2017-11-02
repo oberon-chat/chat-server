5a7c53d1f2cab19523aaf147c951b2b70f6e1171
Hailey Willis <yourFriendWes@users.noreply.github.com>
2017-11-01 18:13:00 -0700

    Simplify GetStarredMessages.call

--- apps/chat_server/lib/chat_server/contexts/starred_messages/get_starred_messages.ex
+++ apps/chat_server/lib/chat_server/contexts/starred_messages/get_starred_messages.ex
@@ -2,8 +2,6 @@ defmodule ChatServer.GetStarredMessages do
   alias ChatServer.Schema
 
   def call(%Schema.User{} = user) do
-    case Schema.StarredMessage.get_by_user(user) do
-      starred_messages -> {:ok, starred_messages}
-    end
+    Schema.StarredMessage.get_by_user(user)
   end
 end
