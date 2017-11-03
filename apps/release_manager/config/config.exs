4be36717d709efeb8d5c0a34f380004821ae35c6
Chris Laskey <contact@chrislaskey.com>
2017-11-03 16:14:02 -0400

    Fix release manager reference

--- apps/release_manager/config/config.exs
+++ apps/release_manager/config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :release_manager, apps: [
-  chat_oauth2: ChatServer.OAuth2,
+  chat_oauth2: ChatOAuth2.Repo,
   chat_server: ChatServer.Repo,
   chat_webhook: ChatWebhook.Repo
 ]
