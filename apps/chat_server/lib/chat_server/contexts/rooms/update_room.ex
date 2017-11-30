3e93eb77376b674939cba3f242886d9e1889c7de
Jack Hefner III <jmhthethird@users.noreply.github.com>
2017-11-29 17:53:45 -0800

    Archive room update (#20)
    * add handle in for archive room
    
    * Correct Typo and change call arg to room_id
    
    * Fix bug calling typo'd attribute
    
    * Use room.id as arg to UpdateState.call
    
    * use get_by instead of get, use more general var names for flexibility
    
    * revert back to get(id) instead of get_by(id)
    
    * handle_in for reactivation of room
    
    * Renaming to UpdateRoom as it only functions to update rooms currently
    
    * refactor broadcast to match new pattern
    
    * Notify subscribers when room has changed (#2)
    
    * Track archived support rooms (#3)
    
    * Add room state to support room presence
    
    * Update support presence if support  room state changes
    
    * Update support state after reactivate room
    
    * Update support state after reactivate room (#4)
    
    * Change function name and handle_in call key name
    
    * Use GetRoom so tuple is returned
    
    * update getroom to include call
    
    * fix syntax to remove single additional space
    
    * Fix indentation

--- apps/chat_server/lib/chat_server/contexts/rooms/update_room.ex
+++ apps/chat_server/lib/chat_server/contexts/rooms/update_room.ex
@@ -1,7 +1,9 @@
-defmodule ChatServer.UpdateState do
+defmodule ChatServer.UpdateRoom do
   require Logger
 
+  alias ChatServer.BroadcastEvent
   alias ChatServer.Schema
+  alias ChatServer.GetRoom
 
   defmodule State do
     @derive {Poison.Encoder, only: [room: [:id, :slug, :name]]}
@@ -9,12 +11,13 @@ defmodule ChatServer.UpdateState do
     defstruct [:room]
   end
 
-  def call(params \\ %{}, _user, state) do
-    Logger.info "Updating room " <> inspect(params)
+  def call(room_id, _user, state) do
+    Logger.info "Updating room " <> inspect(room_id)
 
     # TODO verify user is allowed to update room of that type
 
-    with {:ok, record} <- update(params, state),
+    with {:ok, record} <- GetRoom.call(room_id),
+         {:ok, record} <- update(record, state),
          :ok <- broadcast_update(record) do
       {:ok, record}
     else
@@ -25,14 +28,12 @@ defmodule ChatServer.UpdateState do
   defp update(params, state) do
     changeset = params
     |> Map.take([:id, :name])
-    |> Map.put(:status, state)
+    |> Map.put(:state, state)
 
     Schema.Room.update(params, changeset)
   end
 
   defp broadcast_update(room) do
-    event = %State{room: room}
-
-    ChatPubSub.broadcast! "events", "room:updated", event
+    BroadcastEvent.call("room:updated", room)
   end
 end
